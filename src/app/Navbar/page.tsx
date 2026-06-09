c12657f8da6f58e1b068304bffa505924f9d327c
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-06-09 20:41:54 +0530

    change navbar background color

--- src/app/Navbar/page.tsx
+++ src/app/Navbar/page.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white">
+    <nav className="bg-black text-white">
       <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
         
         <h1 className="text-xl font-bold">MyProject</h1>
 
         <div className="flex gap-5">
-          <Link href="/">Home</Link>
+          <Link href="/" className="">Home</Link>
           <Link href="/about">About</Link>
           <Link href="/services">Services</Link>
           <Link href="/contact">Contact</Link>
