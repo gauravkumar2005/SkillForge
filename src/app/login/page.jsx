506af1bd8cd1f2c83222560e4accd7595513aeee
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-06-11 22:32:09 +0530

    change remember me label in login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -38,7 +38,7 @@ export default function LoginPage() {
           </div>
 
           <div className="flex justify-between items-center text-sm">
-            <label className="flex items-center gap-2">
+            <label className="flex items-center text-blackgap-2">
               <input type="checkbox" />
               Remember Me
             </label>
