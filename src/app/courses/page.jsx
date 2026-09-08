0e814a15181fd0b8d3695704aeb9f8f77bc2c2b7
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-09-08 16:39:07 +0530

    update couses page in Skillforge

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -58,7 +58,7 @@ export default function MyCoursesPage() {
           </div>
 
           <div className="bg-white p-6 rounded-2xl shadow-sm">
-            <h3 className="text-gray-500"></h3>
+            <h3 className="text-gray-500">Certificates</h3>
             <p className="text-3xl text-blue-600 font-bold mt-2">1</p>
           </div>
         </div>
