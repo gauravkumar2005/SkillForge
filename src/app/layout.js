d2490769fe29e35cf044ca83b30344c09357605a
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-09-09 20:04:19 +0530

    add Navbar.jsx in components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Navbar from "../app/navbar/page";
+import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 
 export default function RootLayout({ children }) {
