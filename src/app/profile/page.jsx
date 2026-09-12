5da28cdfba5538cc8b60731ade1c42e626e20bac
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-09-12 14:44:43 +0530

    add hover effect in profile detail section in profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -200,7 +200,7 @@ export default function ProfilePage() {
                   </p>
                 </div>
 
-                <div className="border border-gray-200 rounded-xl p-4">
+                <div className="border border-gray-200 rounded-xl p-4 transition-all duration-300 hover:-translate-y-1 hover:shadow-md hover:border-blue-200  hover:bg-blue-50/30">
                   <p className="text-sm text-gray-500">
                     Learning Hours
                   </p>
